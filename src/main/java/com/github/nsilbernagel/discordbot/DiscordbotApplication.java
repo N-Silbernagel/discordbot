1232e30b3f80ccbe9f61199a8380aacddebe89db
Nils Silbernagel <n.silbernagel@awesome-software.de>
2020-11-16 22:12:22 +0100

    dotenv ignore missing .env file for production

--- src/main/java/com/github/nsilbernagel/discordbot/DiscordbotApplication.java
+++ src/main/java/com/github/nsilbernagel/discordbot/DiscordbotApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiscordbotApplication {
 
   public static void main(String[] args) {
-    Dotenv env = Dotenv.load();
+    Dotenv env = Dotenv.configure().ignoreIfMissing().load();
 
     DiscordClient dClient = DiscordClient.create(env.get("DISCORD_TOKEN"));
     GatewayDiscordClient client = dClient.login().block();
