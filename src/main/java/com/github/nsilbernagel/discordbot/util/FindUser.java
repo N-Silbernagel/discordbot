524564b9ef9c6c60c631b60d5f08f2dd25d4c43e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-26 12:40:59 +0100

    Util class to find a member

---
+++ src/main/java/com/github/nsilbernagel/discordbot/util/FindUser.java
@@ -0,0 +1,22 @@
+package com.github.nsilbernagel.discordbot.util;
+
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FindUser {
+    public static Optional<Member> getUserFromName(final String userID, VoiceChannel channel) {
+        Mono<List<Member>> matchingUsers = channel.getVoiceStates().flatMap(VoiceState::getMember)
+                .filter(member -> member.getId().asString().equals(userID))
+                .collectList();
+        try {
+            return Optional.of(matchingUsers.block().get(0));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+    }
+}
