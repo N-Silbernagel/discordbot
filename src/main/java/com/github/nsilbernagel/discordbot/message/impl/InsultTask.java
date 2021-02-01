bff47d78e83a5c972fb395bf22f2d8da67f68ebe
Nils Silbernagel <n.silbernagel@awesome-software.de>
2021-02-01 09:22:11 +0100

    insult task use webflux

--- src/main/java/com/github/nsilbernagel/discordbot/message/impl/InsultTask.java
+++ src/main/java/com/github/nsilbernagel/discordbot/message/impl/InsultTask.java
@@ -1,56 +1,34 @@
 package com.github.nsilbernagel.discordbot.message.impl;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
 import com.github.nsilbernagel.discordbot.message.IMessageTask;
-import com.github.nsilbernagel.discordbot.message.TaskException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.HtmlUtils;
 
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
 
-public class InsultTask extends AbstractMessageTask implements IMessageTask{
+@Component
+public class InsultTask extends AbstractMessageTask implements IMessageTask {
 
-	public final static String KEYWORD = "beleidige";
-	
-	public boolean canHandle(String keyword) {
-	    return KEYWORD.equals(keyword);
-	 }
-	
-	@Override
-	public void execute(Message message) {  
-	    HttpsURLConnection connection;
-		String insult = null;
-		try {
-			connection = (HttpsURLConnection) new URL("https://evilinsult.com/generate_insult.php?lang=en&type=text").openConnection();
-			connection.setRequestMethod("GET");
-			connection.setConnectTimeout(5000);
-			connection.setReadTimeout(5000);
-			connection.connect();
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String inputLine;
-			StringBuffer content = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				content.append(inputLine);
-			}
-			in.close();
-			insult= content.toString();
-			connection.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		this.message = message;
-		
-		if(insult!=null) {
-			this.answerMessage(insult);
-		}
-		
-	}
+  public final static String KEYWORD = "beleidige";
+
+  public boolean canHandle(String keyword) {
+    return KEYWORD.equals(keyword);
+  }
+
+  @Override
+  public void execute(Message message) {
+    this.message = message;
+
+    String insult = WebClient.create("https://evilinsult.com")
+        .get()
+        .uri("/generate_insult.php?lang=en&type=text")
+        .retrieve()
+        .bodyToMono(String.class)
+        .onErrorReturn("Dicke Qualle")
+        .block();
+
+    this.answerMessage(HtmlUtils.htmlUnescape(insult));
+  }
 }
