86a5f74c472989a5f6ff551696a3f9f2d66958ee
Nils Silbernagel <n.silbernagel@awesome-software.de>
2021-02-09 09:08:43 +0100

    show which audio the bot is playing

--- src/main/java/com/github/nsilbernagel/discordbot/audio/LavaTrackScheduler.java
+++ src/main/java/com/github/nsilbernagel/discordbot/audio/LavaTrackScheduler.java
@@ -3,6 +3,7 @@ package com.github.nsilbernagel.discordbot.audio;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
+import com.github.nsilbernagel.discordbot.DiscordbotApplication;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
@@ -11,11 +12,16 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import discord4j.core.object.presence.Activity;
+import discord4j.core.object.presence.Presence;
+
 @Component
 public class LavaTrackScheduler extends AudioEventAdapter {
 
   @Autowired
   private LavaPlayerAudioProvider lavaPlayerAudioProvider;
+
+  private DiscordbotApplication discordbotApplication;
 
   private final BlockingQueue<AudioTrack> queue;
 
@@ -49,11 +55,22 @@ public class LavaTrackScheduler extends AudioEventAdapter {
   }
 
   @Override
+  public void onTrackStart(AudioPlayer player, AudioTrack track) {
+    this.discordbotApplication.getDiscordClient()
+        .updatePresence(Presence.online(Activity.playing(track.getInfo().title)))
+        .subscribe();
+  }
+
+  @Override
   public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
     // Only start the next track if the end reason is suitable for it (FINISHED or
     // LOAD_FAILED)
     if (endReason.mayStartNext) {
       nextTrack();
+    } else {
+      this.discordbotApplication.getDiscordClient()
+          .updatePresence(Presence.online())
+          .subscribe();
     }
   }
 }
