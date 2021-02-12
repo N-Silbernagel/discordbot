97613522546c5e74314499d7d2cb7f233c4a3fc1
Nils Silbernagel <n.silbernagel@awesome-software.de>
2021-02-12 21:00:41 +0100

    bulk delete task validate param

--- src/main/java/com/github/nsilbernagel/discordbot/message/impl/BulkDeleteTask.java
+++ src/main/java/com/github/nsilbernagel/discordbot/message/impl/BulkDeleteTask.java
@@ -3,7 +3,9 @@ package com.github.nsilbernagel.discordbot.message.impl;
 import com.github.nsilbernagel.discordbot.guard.annotations.NeedsPermission;
 import com.github.nsilbernagel.discordbot.listeners.impl.MessageCreateEventListener;
 import com.github.nsilbernagel.discordbot.message.AbstractMessageTask;
-import com.github.nsilbernagel.discordbot.message.FalseInputException;
+import com.github.nsilbernagel.discordbot.validation.CommandParam;
+import com.github.nsilbernagel.discordbot.validation.rules.annotations.Numeric;
+import com.github.nsilbernagel.discordbot.validation.rules.annotations.Required;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,33 +22,20 @@ public class BulkDeleteTask extends AbstractMessageTask {
   @Autowired
   private MessageCreateEventListener messageCreateEventListener;
 
+  @CommandParam(0)
+  @Required("Bitte gib eine Zahl an.")
+  @Numeric(value = "Bitte gib eine Zahl zwischen 1 und 10 an.", min = 1, max = 10)
+  private long numberOfMessagesToDelete;
+
   public boolean canHandle(String keyword) {
     return KEYWORD.equals(keyword);
   }
 
   @Override
   public void action() {
-    if (this.messageToTaskHandler.getCommandParameters().size() == 0) {
-      throw new FalseInputException("Bitte gib eine Zahl an.");
-    }
-
-    String messagesCountString = this.messageToTaskHandler.getCommandParameters().get(0);
-
-    Integer messagesCount;
-
-    try {
-      messagesCount = Integer.parseInt(messagesCountString);
-    } catch (NumberFormatException e) {
-      throw new FalseInputException("Bitte gib eine Zahl an.");
-    }
-
-    if (messagesCount < 1 || messagesCount > 10) {
-      throw new FalseInputException("Bitte gib eine Zahl zwischen 1 und 10 an.");
-    }
-
     Mono<Void> bulkDeleteMono = this.messageCreateEventListener.getMessageChannel()
         .getMessagesBefore(this.getMessage().getId())
-        .take(messagesCount)
+        .take(numberOfMessagesToDelete)
         .flatMap(foundMessage -> foundMessage.delete())
         .then();
 
