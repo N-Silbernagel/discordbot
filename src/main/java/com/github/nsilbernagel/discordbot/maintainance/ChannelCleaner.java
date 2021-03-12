a94af6ae05745dc791504788f68893662b6491e7
simonjuergens <s.juergens99@gmail.com>
2021-03-12 18:29:45 +0100

    remove unused imports and format code

--- src/main/java/com/github/nsilbernagel/discordbot/maintainance/ChannelCleaner.java
+++ src/main/java/com/github/nsilbernagel/discordbot/maintainance/ChannelCleaner.java
@@ -1,24 +1,22 @@
 package com.github.nsilbernagel.discordbot.maintainance;
 
-import java.time.Instant;
+import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.TextChannel;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.TextChannel;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
 
 @Component
 public class ChannelCleaner {
 
   @Value("${app.discord.command-token:!}")
   private String commandToken;
-
-  Flux<Message> messagesToDelete;
 
   public void execute(TextChannel channel) {
     Flux<Message> messagesToDelete = channel.getMessagesBefore(Snowflake.of(Instant.now()))
@@ -29,7 +27,7 @@ public class ChannelCleaner {
         .blockLast();
   }
 
-  private Boolean checkIfFromBotOrCommand(Message message){
+  private Boolean checkIfFromBotOrCommand(Message message) {
     if (message.getAuthor().isEmpty()) {
       return false;
     }
